7a554230269937fbebe48938bf5230c4e40e8d90
Andrew Ruba <andrewruba@Andrews-MacBook-Pro.local>
2025-06-29 00:26:37 -0400

    classes for pytests

--- tests/test_map.py
+++ tests/test_map.py
@@ -6,44 +6,41 @@ from yassa_bio.schema.layout.plate import Plate
 from yassa_bio.schema.layout.well import Well
 
 
-def _make_plate(pid="P1"):
-    return Plate(
-        plate_id=pid,
-        wells=[Well(well="A1", file_row=0, file_col=0, sample_type="blank")],
-    )
+class TestDataMap:
 
+    def _make_plate(self, pid="P1"):
+        return Plate(
+            plate_id=pid,
+            wells=[Well(well="A1", file_row=0, file_col=0, sample_type="blank")],
+        )
 
-def test_single_plate_ok():
-    plate = _make_plate("EZHNPY-25K")
-    dm = DataMap(plates=[plate])
-    assert len(dm.plates) == 1
-    assert dm.plates[0].plate_id == "EZHNPY-25K"
+    def test_single_plate_ok(self):
+        plate = self._make_plate("EZHNPY-25K")
+        dm = DataMap(plates=[plate])
+        assert len(dm.plates) == 1
+        assert dm.plates[0].plate_id == "EZHNPY-25K"
 
+    def test_multiple_plates_ok(self):
+        plates = [self._make_plate("P1"), self._make_plate("P2")]
+        dm = DataMap(plates=plates)
+        assert [p.plate_id for p in dm.plates] == ["P1", "P2"]
 
-def test_multiple_plates_ok():
-    plates = [_make_plate("P1"), _make_plate("P2")]
-    dm = DataMap(plates=plates)
-    assert [p.plate_id for p in dm.plates] == ["P1", "P2"]
+    def test_empty_plate_list_ok(self):
+        dm = DataMap(plates=[])
+        assert dm.plates == []
+        assert len(dm.plates) == 0
 
+    @pytest.mark.parametrize("bad_item", [123, "not_a_plate", None, {"plate_id": "P"}])
+    def test_non_plate_items_raise(self, bad_item):
+        with pytest.raises(ValidationError):
+            DataMap(plates=[bad_item])
 
-def test_empty_plate_list_ok():
-    dm = DataMap(plates=[])
-    assert dm.plates == []
-    assert len(dm.plates) == 0
-
-
-@pytest.mark.parametrize("bad_item", [123, "not_a_plate", None, {"plate_id": "P"}])
-def test_non_plate_items_raise(bad_item):
-    with pytest.raises(ValidationError):
-        DataMap(plates=[bad_item])
-
-
-def test_nested_plate_validation_bubbles_up():
-    bad_plate_dict = {
-        "plate_id": "Bad",
-        "wells": [
-            {"well": "ZZ0", "file_row": 0, "file_col": 0, "sample_type": "blank"}
-        ],
-    }
-    with pytest.raises(ValidationError):
-        DataMap(plates=[bad_plate_dict])
+    def test_nested_plate_validation_bubbles_up(self):
+        bad_plate_dict = {
+            "plate_id": "Bad",
+            "wells": [
+                {"well": "ZZ0", "file_row": 0, "file_col": 0, "sample_type": "blank"}
+            ],
+        }
+        with pytest.raises(ValidationError):
+            DataMap(plates=[bad_plate_dict])
