97ddb6e158ec930c5663e150dc95e6dc102af0d8
Andrew Ruba <andrewruba@Andrews-MacBook-Pro.local>
2025-06-29 16:10:47 -0400

    delete random docstrings

--- src/yassa_bio/schema/layout/enum.py
+++ src/yassa_bio/schema/layout/enum.py
@@ -2,16 +2,12 @@ from enum import StrEnum, IntEnum
 
 
 class PlateFormat(IntEnum):
-    """Allowed well counts for microplates."""
-
     FMT_96 = 96
     FMT_384 = 384
     FMT_1536 = 1536
 
 
 class SampleType(StrEnum):
-    """High-level role of a well in the assay."""
-
     BLANK = "blank"
     STANDARD = "standard"
     CONTROL = "control"
