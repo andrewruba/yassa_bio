3a7978277c797bd1b2d24b76f3b49acba1638ad1
Andrew Ruba <andrewruba@Andrews-MacBook-Pro.local>
2025-07-06 19:43:07 -0400

    analysis/engine/fit.py placeholder

--- src/yassa_bio/evaluation/analysis/fit.py
+++ src/yassa_bio/evaluation/analysis/fit.py
@@ -1,4 +1,10 @@
-# @register("curve_model", "4PL")
-# class FourPLModel:
-#     def fit(self, x, y, w): …
-#     def predict(self, x): …
+from yassa_bio.core.registry import register
+
+
+@register("curve_model", "4PL")
+class FourPLModel:
+    def fit(self, x, y, w):
+        pass
+
+    def predict(self, x):
+        pass
