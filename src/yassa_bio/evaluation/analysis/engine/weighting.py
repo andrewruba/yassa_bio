d22aad2b44a17a6b867b094400c8e0ca63a855f9
Andrew Ruba <andrewruba@Andrews-MacBook-Pro.local>
2025-07-11 00:54:42 -0400

    underscore indicator in weighting functions that x or y is unused

--- src/yassa_bio/evaluation/analysis/engine/weighting.py
+++ src/yassa_bio/evaluation/analysis/engine/weighting.py
@@ -6,24 +6,29 @@ from yassa_bio.core.registry import register
 
 @register("weighting", Weighting.ONE)
 def weight_one(x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    _ = x
     return np.ones_like(y)
 
 
 @register("weighting", Weighting.ONE_OVER_X)
 def weight_one_over_x(x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    _ = y
     return 1.0 / x
 
 
 @register("weighting", Weighting.ONE_OVER_X2)
 def weight_one_over_x2(x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    _ = y
     return 1.0 / (x**2)
 
 
 @register("weighting", Weighting.ONE_OVER_Y)
 def weight_one_over_y(x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    _ = x
     return 1.0 / y
 
 
 @register("weighting", Weighting.ONE_OVER_Y2)
 def weight_one_over_y2(x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    _ = x
     return 1.0 / (y**2)
